c6fb5d42914372408df2678763c265f65f153873
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-21 17:41:56 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,53 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState, useEffect } from "react";
+import "../styles/App.css";
+import slides from "../data.js";
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const [prevv, setPrevv] = useState(false);
+  const [next, setNext] = useState(false);
+  function handleRestart() {
+    setIndex(0);
+  }
+  function handlePrev() {
+    if (index > 0) {
+      let i = index;
+      setIndex(i - 1);
+    }
+  }
+  function handleNext() {
+    if (index < slides.length - 1) {
+      let i = index;
+      setIndex(i + 1);
+    }
+  }
+  useEffect(() => {
+    if (index === slides.length - 1) {
+      setPrevv(false);
+      setNext(true);
+    } else if (index === 0) {
+      setPrevv(true);
+      setNext(false);
+    } else {
+      setPrevv(false);
+      setNext(false);
+    }
+  }, [index]);
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      <button data-testid="button-restart" onClick={handleRestart}>
+        Restart
+      </button>
+      <button data-testid="button-prev" onClick={handlePrev} disabled={prevv}>
+        Previous
+      </button>
+      <button data-testid="button-next" onClick={handleNext} disabled={next}>
+        Next
+      </button>
+    </>
+  );
+};
 
 export default App;
